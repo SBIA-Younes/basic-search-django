b345af87825648faa9c338274e4aa95fb4d47860
SBIA-Younes <94634321+SBIA-Younes@users.noreply.github.com>
2023-10-27 10:19:29 +0100

    Template Modification

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import HommePagerView, SearchResultsView, BlogDetailView
 
 urlpatterns = [
-    path("", HommePagerView.as_view(),name='homme'),
+    path("", HommePagerView.as_view(),name='home'),
     path('search/',SearchResultsView.as_view(),name='search_results'),
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,12 +9,14 @@ class HommePagerView(ListView):
     model = BlogPost
     template_name = "home.html"
     context_object_name = "blogs"
+    paginate_by = 10
   
   
 class SearchResultsView(ListView):
     model = BlogPost
     template_name = "serach_results.html"
     context_object_name = "blogs"
+    paginate_by = 3
 
     def get_queryset(self) -> QuerySet[Any]:
         query = self.request.GET.get('q')
